4558d08e75996f480e44e07cb4a55c952cacb18f
Hugo Heynard <44136168+hugoheynard@users.noreply.github.com>
2024-09-24 11:27:22 +0200

    Error managing and TU class_DateMethods.js

--- backend/Utilities/class_DateMethods.js
+++ backend/Utilities/class_DateMethods.js
@@ -4,57 +4,91 @@ class DateMethod {
     static STEP_DURATION = 5;
     static today = new Date(Date.now()).toISOString().split('T')[0]; // returns today's date in good format
 
+
+    //PARAMETER VALIDATION
+    static check_dateIsInstanceOfDate(date) {
+
+        if (Array.isArray(date)) {
+            const faulty = date.find(elem => !(elem instanceof Date))
+
+            if (faulty) {
+                throw new Error(`All the "date" elements must be an instance of Date`);
+            }
+            return;
+        }
+
+        if (!(date instanceof Date)) {
+            throw new Error('The "date" parameter must be an instance of Date');
+        }
+    };
+    static check_positiveIntegerParameter(number) {
+
+        if (number <= 0 || !Number.isInteger(number)) {
+            throw new Error('parameter must be positive integer only');
+        }
+    };
+
+
     //TIME MANIPULATIONS
     static addMinutes(date, minutes){
-        if (minutes <= 0 || !Number.isInteger(minutes)) {
-            throw new Error('minutes parameter must be positive integer only');
-        }
+        DateMethod.check_dateIsInstanceOfDate(date);
+        DateMethod.check_positiveIntegerParameter(minutes);
 
         const newDate = new Date(date);
         newDate.setMinutes(newDate.getMinutes() + minutes);
         return newDate;
     };
-    static substractMinutes = (date, minutes) => {
-        if (minutes <= 0 || !Number.isInteger(minutes)) {
-            throw new Error('minutes parameter must be positive integer only');
-        }
+    static substractMinutes(date, minutes){
+        DateMethod.check_dateIsInstanceOfDate(date);
+        DateMethod.check_positiveIntegerParameter(minutes);
 
         const newDate = new Date(date);
         newDate.setMinutes(date.getMinutes() - minutes);
         return newDate;
     };
     static addDays(date, days){
-        let result = new Date(date);
-        result.setDate(result.getDate() + days);
-        return result;
-    };
-    static roundedTime(time, nearest = DateMethod.STEP_DURATION) {
-        return (Math.round(time / nearest) * nearest) % 60;
-    };
+        DateMethod.check_dateIsInstanceOfDate(date);
+        DateMethod.check_positiveIntegerParameter(days);
 
+        const newDate = new Date(date);
+        newDate.setDate(newDate.getDate() + days);
+
+        return newDate;
+    };
     static startOfDay(date) {
-
-        if (!date instanceof Date) {
-            throw new Error('date parameter must be instance of Date');
-        }
+        DateMethod.check_dateIsInstanceOfDate(date);
 
         return new Date(date.setHours(0, 0, 0, 0));
     };
     static endOfDay(date) {
+        DateMethod.check_dateIsInstanceOfDate(date);
+
         return DateMethod.addDays(DateMethod.startOfDay(date), 1);
+    };
+
+    static roundedTime(time, nearest = DateMethod.STEP_DURATION) {
+        return (Math.round(time / nearest) * nearest) % 60;
     };
 
     //TIME COMPARISONS
     static differenceInMinutes(closestDate, furthestDate) {
+        //DateMethod.check_dateIsInstanceOfDate([closestDate, furthestDate]);
         return (furthestDate - closestDate) / DateMethod.ONE_MINUTE_IN_MS;
     };
     static sameDates = (date1, date2) => {
+        DateMethod.check_dateIsInstanceOfDate([date1, date2]);
+
         return date1.getTime() === date2.getTime();
     };
     static inBetweenDates = (date, startDate, endDate) => {
+        DateMethod.check_dateIsInstanceOfDate([date, startDate, endDate]);
         return date >= startDate && date <= endDate;
     };
-    static indexOfDay = date =>  new Date(date).getDay();
+    static indexOfDay = date => {
+        DateMethod.check_dateIsInstanceOfDate(date);
+
+        return new Date(date).getDay()
+    };
 
     //TIME LAYOUT
     static standardizeTime = time => {
--- backend/Utilities/utilities_UT/class_DateMethods.test.js
+++ backend/Utilities/utilities_UT/class_DateMethods.test.js
@@ -3,13 +3,52 @@ import {DateMethod} from "../class_DateMethods.js";
 describe ('class DateMethods', () => {
     const inputDate = new Date('2024-01-01T00:00:00');
 
-    describe('Method - addMinutes', () => {
 
-        test('throws an error if "minutes" parameter <= 0', ()=> {
-            expect(()=> DateMethod.addMinutes(inputDate,0)).toThrow('minutes parameter must be positive integer only');
-            expect(()=> DateMethod.addMinutes(inputDate,-1)).toThrow('minutes parameter must be positive integer only');
-            expect(()=> DateMethod.addMinutes(inputDate, 1.5)).toThrow('minutes parameter must be positive integer only');
+    // check methods for input parameters validation
+    describe ('Method - check_dateIsInstanceOfDate', () => {
+
+        test('should throw an error if the date parameter is not an instance of Date', () => {
+            const validSingleDate = new Date('2024-01-01T14:45:00');
+            const unvalidSingleDate = '2024-01-01T00:00:00';
+
+            const validArray = [new Date('2024-01-01T14:45:00'), new Date('2024-01-02T14:45:00')]
+            const unvalidArray = [new Date('2024-01-01T14:45:00'), '2024-01-02T14:45:00']
+
+            //check single date cases
+            expect(() => DateMethod.check_dateIsInstanceOfDate(unvalidSingleDate))
+                .toThrowError('The "date" parameter must be an instance of Date');
+
+            expect(() => DateMethod.check_dateIsInstanceOfDate(validSingleDate))
+                .not.toThrow();
+
+            //check array of date cases
+            expect(() => DateMethod.check_dateIsInstanceOfDate(unvalidArray))
+                .toThrowError(`All the "date" elements must be an instance of Date`);
+
+            expect(() => DateMethod.check_dateIsInstanceOfDate(validArray))
+                .not.toThrow();
         });
+    });
+    describe('Method - check_positiveIntegerParameter', () => {
+
+        test('throws an error if number parameter <= 0 or is not an integer', ()=> {
+            const invalidValues = [0, -1, 1.5];
+            const validValue = 2;
+
+            // Test for invalid values
+            for (const value of invalidValues) {
+                expect(() => DateMethod.check_positiveIntegerParameter(value))
+                    .toThrow('parameter must be positive integer only');
+            }
+
+            // Test for valid value
+            expect(() => DateMethod.check_positiveIntegerParameter(validValue)).not.toThrow();
+        });
+    });
+
+
+    //date methods
+    describe('Method - addMinutes', () => {
 
         test('should return an instance of date incremented by a number of minutes', ()=> {
 
@@ -21,8 +60,33 @@ describe ('class DateMethods', () => {
             expect(result).toEqual(expectedDate);
             expect(result).toBeInstanceOf(Date)
         });
-    })
+    });
+    describe('Method - substractMinutes', () => {
 
+        test('should return an instance of date decremented by a number of minutes', ()=> {
+
+            const timeToSub = 5;
+            const expectedDate = new Date('2023-12-31T23:55:00');
+
+            const result = DateMethod.substractMinutes(inputDate, timeToSub);
+
+            expect(result).toEqual(expectedDate);
+            expect(result).toBeInstanceOf(Date)
+        });
+    });
+    describe('Method - addDays', () => {
+
+        test('should return an instance of date incremented by a number of day', ()=> {
+
+            const daysToAdd = 2;
+            const expectedDate = new Date('2024-01-03T00:00:00');
+
+            const result = DateMethod.addDays(inputDate, daysToAdd);
+
+            expect(result).toEqual(expectedDate);
+            expect(result).toBeInstanceOf(Date)
+        });
+    });
     describe ('Method - startOfDay', () => {
         test('should take a date and return the earliest time of the day (00:00:00)', () => {
             const inputDate = new Date('2024-01-01T14:45:00');
@@ -33,7 +97,6 @@ describe ('class DateMethods', () => {
             expect(result).toEqual(expectedDate);
         });
     });
-
     describe ('Method - endOfDay', () => {
         test('should return the earliest time of the next day', () => {
             const expectedDate = new Date('2024-01-02T00:00:00');
