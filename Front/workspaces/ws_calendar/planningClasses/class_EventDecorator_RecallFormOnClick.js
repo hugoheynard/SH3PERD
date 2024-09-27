413dc2a9a0e04fafa7a6e45a0b8973160b127b51
Hugo Heynard <44136168+hugoheynard@users.noreply.github.com>
2024-09-27 09:48:24 +0200

    Calendar Front end GridBlock et recall event form on block

---
+++ Front/workspaces/ws_calendar/planningClasses/class_EventDecorator_RecallFormOnClick.js
@@ -0,0 +1,17 @@
+import {appWorkspace} from "../../../script.js";
+import {wsPopWindow_AddEvent} from "../wsCal_PopWindows/wsPopWindow_AddEventWindow.js";
+
+
+export class EventDecorator_RecallFormOnClick {
+    constructor(input) {
+        this.eventsBlock = input.eventsBlock;
+        this.addEventListener_onClick_formCallback(this.eventsBlock);
+    };
+    addEventListener_onClick_formCallback(eventsBlock) {
+        // double click triggers the form callback to modify event block
+        for (const block of eventsBlock) {
+            const block_id = block.blockData._id;
+            block.htmlElement.addEventListener('dblclick', () => appWorkspace.workSpaceStrategy.popContext.setPopMenu(wsPopWindow_AddEvent(block_id)))
+        } //TODO: better access to setpopmenu
+    };
+}
--- Front/workspaces/ws_calendar/planningClasses/class_GridBlock/class_GridBlock.js
+++ Front/workspaces/ws_calendar/planningClasses/class_GridBlock/class_GridBlock.js
@@ -1,10 +1,8 @@
 import {addBlockTitle, addTime} from "./addBlockContent.js";
 import {HTMLelem} from "../../../../frontElements/Classes/HTMLClasses/class_HTMLelem.js";
-import {dragOver, dragStart} from "../../../../Utilities/DragNDropFunctions/dragAndDrop.js";
 
 
 class GridBlock {
-
     constructor(input) {
         this._blockData = input.blockData;
 
@@ -13,21 +11,17 @@ class GridBlock {
         this.htmlElement.setAttribute('draggable', true);
         this.blockResume = new HTMLelem('div', undefined, 'block_resume').render();
 
-        this.addBlockTypeInDataset();
+        this.addDataset();
         this.addBlockResume();
-        //addContent
-
     };
 
     get blockData(){
         return this._blockData;
     };
 
-    addBlockTypeInDataset() {
-        this.htmlElement.dataset.id = this.blockData.id;
+    addDataset() {
+        this.htmlElement.dataset.id = this.blockData._id;
         this.htmlElement.dataset.type = this.blockData.type;
-
-
     };
 
     addBlockResume() {
@@ -37,49 +31,9 @@ class GridBlock {
         this.htmlElement.appendChild(this.blockResume);
     };
 
-    squaredMiniBlock(width) {
-        //we get the height of a row in the grid and insert it as width of this block;
-        this.block.style.width = width;
-
-    }
-
-    resizeListener() {
-        class ResizeController {
-            constructor(block) {
-                this.block = block;
-                this.addResizeObserver();
-            }
-
-            addResizeObserver() {
-                const resizeObserver = new ResizeObserver((entries) => {
-                    for (let entry of entries) {
-                        console.log('Element resized:', entry.target);
-                        console.log('x', entry.contentRect.height);
-                        // Your resize logic here
-                    }
-                });
-
-                // Start observing the target element
-                resizeObserver.observe(this.block);
-            }
-        }
-
-        const resizeController = new ResizeController(this.block);
-    }
-
     renderBlock() {
         this.htmlElement.innerHTML = '';
 
-        //this.block.addEventListener('dragover', dragOver)
-        //this.block.addEventListener('dragover', dragStart)
-
-
-
-
-        //resizertest//TODO: RESIZER
-        //this.resizeListener();
-
-        //return element gridBlock
         return this.htmlElement;
     };
 }
