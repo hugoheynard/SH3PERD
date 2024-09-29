78bced9de867f25b85208924c951b24ded11e5f8
Hugo Heynard <44136168+hugoheynard@users.noreply.github.com>
2024-09-29 21:07:58 +0200

    Delete useless

--- Front/workspaces/ws_calendar/wsPage_Calendar.js
+++
@@ -1,6 +0,0 @@
-import {ViewContext} from "./planningClasses/class_ViewContext.js";
-import {BackEndCall} from "../../frontElements/Classes/class_BackEndCalls.js";
-
-
-export const calendarViewContext = await new ViewContext(await BackEndCall.getDay('2024-12-19'));
-export const wsPage_Calendar = async () => calendarViewContext.render();
