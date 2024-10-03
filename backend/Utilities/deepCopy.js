021d7eab8af4bfc930654101234090f41720fd5c
Hugo Heynard <44136168+hugoheynard@users.noreply.github.com>
2024-10-03 10:35:23 +0200

    deep copy function

---
+++ backend/Utilities/deepCopy.js
@@ -0,0 +1,18 @@
+export const deepCopy = obj => {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    if (Array.isArray(obj)) {
+        return obj.map(deepCopy);
+    }
+
+    const copy = {};
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            copy[key] = deepCopy(obj[key]);
+        }
+    }
+
+    return copy;
+}
