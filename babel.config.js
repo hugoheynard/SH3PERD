104934d57abaff650fd77ac315a3d55e5393b217
Hugo Heynard <44136168+hugoheynard@users.noreply.github.com>
2024-10-03 10:13:41 +0200

    change in config files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
-export default {
-    presets: ['@babel/preset-env'],
+export default  {
+    presets: ['@babel/preset-env', { targets: { node: 'current' } }],
+    plugins: [
+        "@babel/plugin-transform-private-methods",
+        "@babel/plugin-transform-class-properties"
+    ]
 };
