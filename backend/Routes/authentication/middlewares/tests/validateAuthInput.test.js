70667b2cb67d7c4d3c5a5d92b996e61d7821b7f5
Hugo Heynard <44136168+hugoheynard@users.noreply.github.com>
2024-10-17 19:03:39 +0200

    update test

--- backend/Routes/authentication/middlewares/tests/validateAuthInput.test.js
+++ backend/Routes/authentication/middlewares/tests/validateAuthInput.test.js
@@ -1,6 +1,9 @@
+import { jest } from '@jest/globals';
 import express from "express";
 import request from 'supertest';
-import {validateAuthInput} from "../validateAuthInput.js";
+import { validateAuthInput } from "../validateAuthInput.js";
+import {expect} from "@jest/globals";
+
 
 describe('validateAuthInput - login middleware, ', () => {
     const app = express();
@@ -10,10 +13,13 @@ describe('validateAuthInput - login middleware, ', () => {
         res.status(200).json({ message: 'Success' });
     });
 
+    const testMail = 'Test@example.com';
+    const testPass = '12345 ';
+
     test ('should return 401 if missing email', async () => {
         const response = await request(app)
             .post('/test')
-            .send({ password: '12345' });
+            .send({ password: testPass });
 
         expect(response.status).toBe(401);
         expect(response.body.message).toBe('Missing email');
@@ -22,7 +28,7 @@ describe('validateAuthInput - login middleware, ', () => {
     test ('should return 401 if missing password', async () => {
         const response = await request(app)
             .post('/test')
-            .send({ email: 'test@example.com' });
+            .send({ email: testMail });
 
         expect(response.status).toBe(401);
         expect(response.body.message).toBe('Missing password');
@@ -31,10 +37,31 @@ describe('validateAuthInput - login middleware, ', () => {
     test('should call next middleware when email and password are provided', async () => {
         const response = await request(app)
             .post('/test')
-            .send({ email: 'test@example.com', password: '12345' });
+            .send({ email: testMail, password: testPass });
 
         expect(response.status).toBe(200);
         expect(response.body.message).toBe('Success');
     });
 
+
+    test('should trim and lowercase email, trim password, and call next', async () => {
+        const req = {
+            body: {
+                email: '  Test@Example.com ',
+                password: '  12345  '
+            }
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        const next = jest.fn();
+
+
+        validateAuthInput(req, res, next);
+
+        expect(req.body.email).toBe('test@example.com');
+        expect(req.body.password).toBe('12345');
+        expect(next).toHaveBeenCalled();
+    });
 });
