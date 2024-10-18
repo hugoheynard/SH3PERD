13f1f5138a212fffb91ebdf37c2364891bbbc66b
Hugo Heynard <44136168+hugoheynard@users.noreply.github.com>
2024-10-18 17:13:24 +0200

    landpage with basic form

---
+++ Front/frontElements/Forms/appForms/authentificationForms/loginForm.js
@@ -0,0 +1,57 @@
+import {Form} from "../../FormBuilder/class_Form.js";
+import {Login_BackendCall} from "../../../../backendCalls/Login_BackendCall.js";
+import {FormField_textInput} from "../../FormBuilder/Fields/class_FormField_TextInput.js";
+
+export const loginForm = new Form({
+    id: 'loginForm',
+    css: '',
+    submitAction: Login_BackendCall.login
+});
+
+const sectionStyle = {
+    global: 'col gapLarge',
+    header: 'row spaceBetween',
+    title: 'form_sectionTitle',
+    container: 'formSection'
+};
+
+loginForm.addSection({
+    id:'login',
+    positionInForm: 1,
+    element: {
+        title:'',
+        style: sectionStyle
+    }
+})
+
+loginForm.addField(
+    {
+        section:'login',
+        positionInSection: 1,
+        element:new FormField_textInput(
+            {
+                id: 'email',
+                css: 'field textField widthLarge avg',
+                require: false,
+                placeholderContent: 'email'
+            }
+        )
+    }
+);
+
+loginForm.addField(
+    {
+        section:'login',
+        positionInSection: 2,
+        element:new FormField_textInput(
+            {
+                id: 'password',
+                css: 'field textField widthLarge avg ',
+                require: false,
+                placeholderContent: 'password'
+            }
+        )
+    }
+);
+
+loginForm.add_submitButton('Join the herd');
--- Front/script.js
+++ Front/script.js
@@ -2,6 +2,7 @@ import {ws_Calendar} from "./frontElements/init/init_appWorkspaces.js";
 import {HTMLelem} from "./frontElements/Classes/HTMLClasses/class_HTMLelem.js";
 import {menu_appGeneral} from "./menu_appGeneral.js";
 import {WorkSpaceContext} from "./workspaces/navigationArchitecture/class_WorkspaceContext.js";
+import {loginForm} from "./frontElements/Forms/appForms/authentificationForms/loginForm.js";
 
 
 export const appWorkspace = new WorkSpaceContext(
@@ -12,7 +13,7 @@ export const appWorkspace = new WorkSpaceContext(
 const body = document.querySelectorAll('body')[0];
 
 //insert the App left menu
-body.appendChild(menu_appGeneral.render());
+//body.appendChild(menu_appGeneral.render());
 
 const mainContainer = new HTMLelem('div', 'mainContainer').render();
 body.appendChild(mainContainer);
@@ -23,7 +24,35 @@ topMenu.appendChild(new HTMLelem('div').render());
 const meIcon = new HTMLelem('span', 'iconMe').render();
 meIcon.textContent = 'H';
 topMenu.appendChild(meIcon);
-mainContainer.appendChild(topMenu);
+//mainContainer.appendChild(topMenu);
 
-mainContainer.appendChild(appWorkspace.render());
+//mainContainer.appendChild(appWorkspace.render());
+
+export class loginPage {
+    constructor(input) {
+        this.loginForm = input.loginForm;
+
+        this.html = new HTMLelem('div', 'loginPage', '').render();
+        this.addLogo();
+        this.addLoginForm({ form: this.loginForm.render() });
+    };
+    addLogo() {
+        this.logoContainer = new HTMLelem('div', 'logoContainer').render();
+        const logo = new HTMLelem('img')
+        logo.setAttributes({src: '../Front/Public/logo/FullLogo_Transparent_NoBuffer.png'})
+        this.logoContainer.appendChild(logo.render())
+        this.html.appendChild(this.logoContainer);
+    }
+
+    addLoginForm(input) {
+        this.login_formContainer = new HTMLelem('div', 'login_formContainer').render();
+        this.login_formContainer.appendChild(input.form);
+        this.html.appendChild(this.login_formContainer);
+    };
+}
+
+
+
+mainContainer.appendChild(new loginPage({ loginForm: loginForm}).html);
+
 
