7a716817c3330b9b418615fa551ef1ffca9ec4d2
Hugo Heynard <hheynard@gmail.com>
2024-11-03 15:32:39 +0100

    app component

--- shepherd/src/app/components/app/app.component.ts
+++ shepherd/src/app/components/app/app.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, effect, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {AppMenuComponent} from '../menus/appMenu/app-menu.component';
 import {MatSidenav, MatSidenavContainer} from '@angular/material/sidenav';
 import {HeaderComponent} from '../header/header.component';
+import {FooterComponent} from '../footer/footer.component';
+import {AuthService} from "../../services/auth.service";
+import {RouteService} from '../../services/route.service';
+import {NgIf} from '@angular/common';
 
 
 @Component({
@@ -10,14 +14,24 @@ import {HeaderComponent} from '../header/header.component';
   standalone: true,
   imports: [
     RouterOutlet,
-    AppMenuComponent,
-    MatSidenavContainer,
-    MatSidenav,
-    HeaderComponent
+    NgIf,
+    AppMenuComponent, HeaderComponent, FooterComponent,
+    MatSidenavContainer, MatSidenav,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent{
+  public authService = inject(AuthService);
+  public routeService = inject(RouteService);
+
   title = 'shepherd';
+
+  constructor() {
+    effect(async () => {
+      if (!this.authService.isAuthenticatedSignal()) {
+        await this.routeService.navigateToLogin();
+      }
+    });
+  }
 }
