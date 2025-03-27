584a03f2f336b77cd8fc9fefdb39cdf0d998e015
Hugo Heynard <hheynard@gmail.com>
2025-03-27 08:53:30 +0100

    @sh3pherd/passwordManager :
    better type of isRehashDueFromLastHashDate.ts
    + jsDoc
    
    Signed-off-by: Hugo Heynard <hheynard@gmail.com>

--- packages/password-hash-manager/src/utils/isRehashDueFromLastHashDate.ts
+++ packages/password-hash-manager/src/utils/isRehashDueFromLastHashDate.ts
@@ -1,4 +1,19 @@
-export const isRehashDueFromLastHashDate = (input: { lastHashDate: string; rehashAfterDays: number }): boolean => {
+import type {TVerifyLastHashDateFunction} from "../../types/Interfaces";
+
+
+/**
+ * Determines whether a password should be rehashed based on the age of the last hash.
+ *
+ * It calculates the number of days since the `lastHashDate` and compares it
+ * to the `rehashAfterDays` threshold.
+ *
+ * @param input - An object containing:
+ *   - `lastHashDate` (string): The ISO date string when the password was last hashed ex: '2025-12-31';
+ *   - `rehashAfterDays` (number): The number of days after which a password should be rehashed
+ *
+ * @returns `true` if the hash is old enough to require rehashing, otherwise `false`.
+ */
+export const isRehashDueFromLastHashDate = TVerifyLastHashDateFunction = (input) => {
     const { lastHashDate, rehashAfterDays } = input;
 
     const parsedDate = new Date(lastHashDate);
